07604a4f5c73006589571987238818d1a5b0d132
Nathan <nathan@nathandarker.it>
2023-01-07 22:14:50 +0000

    initial commit

--- Transaction Information.sql
+++ Transaction Information.sql
@@ -1,7 +1,10 @@
 SELECT 
     OBJECT_NAME(OBJECT_ID) AS TableName,
     last_user_update As LastUpdated, *
-FROM sys.dm_db_index_usage_stats
-WHERE database_id = DB_ID( 'NorthWind')
-ORDER BY LastUpdated
+FROM 
+    sys.dm_db_index_usage_stats
+WHERE 
+    database_id = DB_ID( 'NorthWind')
+ORDER BY 
+    LastUpdated
 DESC;
